0657999dcdaa1a0e9046d0f0d2e35cf242d8121d
gabriel <nune.gabriel.gn@gmail.com>
2022-07-19 11:26:57 +0000

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=gabriel
+export POSTGRES_PASSWORD=lgcu720x
+export POSTGRES_HOST=gabriel.ccmxicjyjs37.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=uda-3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
